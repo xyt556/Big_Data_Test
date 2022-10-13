96988525f31770eb900c28024339c38fb23d9c7e
xyt556 <xyt556@gmail.com>
2022-10-13 09:58:14 +0800

    1

--- numpy&pandas/11_pandas_intro.py
+++ numpy&pandas/11_pandas_intro.py
@@ -12,9 +12,13 @@ import numpy as np
 
 s = pd.Series([1,3,6,np.nan,4,1]) # similar with 1D numpy
 print(s)
+print('-------------------------')
 dates = pd.date_range('20160101', periods=6)
+print(dates)
+print('-------------------------')
 df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=['A','B','C','D'])
 print(df['B'])
+print('-------------------------')
 df2 = pd.DataFrame({'A' : 1.,
                        'B' : pd.Timestamp('20130102'),
                         'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
