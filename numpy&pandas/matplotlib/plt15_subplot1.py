4638aa4f388c9bb747c068768963cc08ac0e8f84
Yantao XI <xyt556@gmail.com>
2022-11-02 17:14:07 +0800

    1

--- numpy&pandas/matplotlib/plt15_subplot1.py
+++ numpy&pandas/matplotlib/plt15_subplot1.py
@@ -1,7 +1,10 @@
 # 位置是由三个整型数值构成，第一个代表行数，第二个代表列数，第三个代表索引位置。
 # 举个列子：plt.subplot(2, 3, 5) 和 plt.subplot(235) 是一样一样的。
 # 需要注意的是所有的数字不能超过10。
-
+# 第二个参数：projection : {None, ‘aitoff’, ‘hammer’, ‘lambert’, ‘mollweide’, ‘polar’, ‘rectilinear’, str}, optional
+# The projection type of the subplot (Axes). str is the name of a costum projection, see projections. The default None results in a ‘rectilinear’ projection.
+# 可选参数：可以选择子图的类型，比如选择polar，就是一个极点图。默认是none就是一个线形图。
+# https://blog.csdn.net/gailj/article/details/122149994?spm=1001.2101.3001.6650.10&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-10-122149994-blog-106784489.pc_relevant_aa&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-10-122149994-blog-106784489.pc_relevant_aa&utm_relevant_index=12
 import matplotlib.pyplot as plt
 import numpy as np
 
